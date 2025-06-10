defcefdceea25e38637eb373287396ca06c5730f
Kishansharma007 <kishan76899@gmail.com>
2025-06-10 15:24:21 +0530

    global.css  customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return(
-    <div>
-       <Button variant="link">Hello World!</Button>
+    <div className="flex flex-col gap-y-4 p-4">
+      <div>
+        <Button variant="elevated">Hello World!</Button>
+      </div>
+      <div>
+        <Input placeholder="Type something..." />
+      </div>
+      <div>
+        <Progress value={50} />
+      </div>
+      <div>
+        <Textarea placeholder="Type something..." />
+      </div>
+      <div>
+        <Checkbox />
+      </div>
     </div>
   )
 }
